9bf88e8f17ea12c3d69d61f0b1a55f161b63dfe0
Anjali <Anjaliyemmanur@gmail.com>
2026-01-31 20:05:23 +0530

    Update 01_data_understanding.sql

--- sql_queries/01_data_understanding.sql
+++ sql_queries/01_data_understanding.sql
@@ -1,1 +1,30 @@
+**Purpose: To Understand data structure & coverage**
+  
+Tables used:
+customers
+orders
+geolocation
+
+1.Data type of all columns in the “customers” table.    
+Select column_name,data_type from
+targetsql.INFORMATION_SCHEMA.COLUMNS 
+where table_name='customers'
+
+2.Get the time range between which the orders were placed.
+ SELECT MIN(order_purchase_timestamp) as Min_order_time,
+        MAX(order_purchase_timestamp) as Max_order_time,
+        FROM `targetsql.orders`
+
+3.Count the Cities & States of customers who ordered during the given period.
+Select count(distinct c.customer_city) as count_of_cities, 
+count(distinct c.customer_state) as count_of_states 
+from `targetsql.customers` c 
+inner join `targetsql.orders` o 
+on c.customer_id=o.customer_id ;
+
+
+
+
+
+
 
