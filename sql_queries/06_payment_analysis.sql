aa2cbd8876be5807c495dcae0791d8465ea5055e
Anjali <Anjaliyemmanur@gmail.com>
2026-01-31 23:11:03 +0530

    Add payment analysis queries to SQL file
    Added SQL queries to analyze customer payment behavior, including month-on-month orders by payment type and orders by installment count.

--- sql_queries/06_payment_analysis.sql
+++ sql_queries/06_payment_analysis.sql
@@ -1,1 +1,27 @@
+**Purpose: Customer payment behavior**
+
+Tables used:
+payments
+orders
+
+Queries here:
+Month-on-month orders by payment type
+Orders by installment count
+
+1.Month-on-month orders by payment type
+Select extract(year from o.order_purchase_timestamp) as year,
+extract(month from o.order_purchase_timestamp) as month,
+p.payment_type, count(distinct o.order_id) as number_orders
+from targetsql.orders o 
+join targetsql.payments p on o.order_id = p.order_id
+group by year, month, p.payment_type
+order by year, month, p.payment_type;
+
+2.Select payment_installments AS installments,
+count(order_id) as num_orders,
+from `targetsql.payments`
+where payment_installments >= 1
+group by payment_installments
+order by num_orders DESC
+
 
