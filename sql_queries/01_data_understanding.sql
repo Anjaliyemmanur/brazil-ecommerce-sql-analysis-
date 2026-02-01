cc584113c68931c91b4698c18eb9ef53a9ea94f5
Anjali <Anjaliyemmanur@gmail.com>
2026-02-01 11:16:50 +0530

    Update 01_data_understanding.sql

--- sql_queries/01_data_understanding.sql
+++ sql_queries/01_data_understanding.sql
@@ -20,7 +20,23 @@ Select count(distinct c.customer_city) as count_of_cities,
 count(distinct c.customer_state) as count_of_states 
 from `targetsql.customers` c 
 inner join `targetsql.orders` o 
-on c.customer_id=o.customer_id ;
+on c.customer_id=o.customer_id 
+
+Insights:
+Customer IDs are stored as text rather than numbers, as they are used only to uniquely identify customers.
+The zip code field is numeric in format but represents a geographic label.
+City and state are stored as text fields, which may introduce minor inconsistencies in naming locations.
+The data set spans around 2.1 years of orders. This can give some historial insights into customer behavior, repeated purchases etc, buying trends and seasonality.
+
+💡 Recommendations
+Use customer_unique_id when doing customer-level analysis to avoid double counting.
+Treat zip codes as categories for grouping or mapping, not as numeric values.
+Perform customer cohort analysis to understand customer behavior across different segments and evaluate recency, frequency, and purchasing patterns.
+Customer base is geographically diverse.
+Orders are from diverse locations, which means there is wide market reach.
+Indicates potential presence of regional demand patterns.
+
+
 
 
 
