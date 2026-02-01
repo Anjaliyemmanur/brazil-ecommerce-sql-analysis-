6daadf212d7e5270a27c09b9e8e0cb78f3d07a04
Anjali <Anjaliyemmanur@gmail.com>
2026-02-01 11:50:18 +0530

    Update 03_geographical_analysis.sql

--- sql_queries/03_geographical_analysis.sql
+++ sql_queries/03_geographical_analysis.sql
@@ -23,6 +23,16 @@ Order by c.customer_state, month_dt ;
 Select c.customer_state,  
 COUNT(DISTINCT c.customer_unique_id) AS total_customers FROM
 `targetsql.customers` c GROUP BY c.customer_state                    
-Order by total_customers DESC
+Order by total_customers desc
 
+Insights-
+1.Order volumes show clear monthly seasonality rather than a flat distribution.Mid-year months (May–August) experience higher order volumes, indicating peak demand periods.
+A noticeable dip around September–October suggests seasonal slowdown in customer purchasing.End-of-year recovery hints at festive or promotional-driven demand.
+  
+2.One can identify the states with the highest number of customers like SP, and increase promotional activities in such similar states with high order volumes to
+capitalize on existing demand and increase sales.
+  
+Recommendations-
+1.The business should see whats working for those states, and try to implement tailored products or so with the states that have lesser customers. 
+2.Use targeted promotions and discounts during low-demand months to smooth revenue.
 
